a089634d2686b4c5802c7ba123ff48832c9a4e2e
IGAZIEV <islam.gaziev@gmail.com>
2022-08-03 18:38:41 +0500

    Module #3. Task #4. Attempt #1.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+            int a = 9;
+            int b = 10;
+            float formula = (9 * a ^ 2 + 5 * b + 2 + a + 7) * ((a + b - 4 * a * b) / 2);
+            System.out.println(formula);
     }
 }
